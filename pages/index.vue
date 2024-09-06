be1e217b86ea723c50f94191e1d601c9c871993a
Janshawn <janshawns@gmail.com>
2024-09-06 08:58:27 +0800

    Test

--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,12 @@
 // emits
 
 // 自訂變數
-const data = ref('')
-const res = await $fetch('/llm/getLLMList')
-data.value = res
 
 // 自訂函式
 
 // lifeCycle
 </script>
 <template>
-  <h2>LLM清單：</h2>
-  <div v-for="item in data">{{ item }}</div>
+  <div>我這是直接修改推上git Hub</div>
 </template>
 <style scoped></style>
